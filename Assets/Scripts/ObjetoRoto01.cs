dadec10f443a013cd570d8ec8bf12c1fbc37cc22
SantiGG04 <60526538+SantiGG04@users.noreply.github.com>
2020-02-01 14:15:27 -0300

    Sistema de Fallas y Falla Gravedad

--- Assets/Scripts/ObjetoRoto01.cs
+++ Assets/Scripts/ObjetoRoto01.cs
@@ -5,6 +5,8 @@ using UnityEditor.UI;
 
 public class ObjetoRoto01 : MonoBehaviour
 {
+    public string sistemaDe;
+
     public GameObject Cartel;
     public GameObject contenedorPieza01;
     public GameObject contenedorPieza02;
@@ -16,36 +18,83 @@ public class ObjetoRoto01 : MonoBehaviour
     public GameObject tildePieza02;
     public GameObject tildePieza03;
 
+    public bool error = false;
+    public bool roto = false;
+    public float tiempoParaErrorOriginal;
+    public float tiempoParaError;
+    public float tiempoParaRomperOriginal;
+    public float tiempoParaRomper;
+    public bool errorEjecutado;
+
+    public void Start()
+    {
+        tiempoParaError = tiempoParaErrorOriginal;
+        tiempoParaRomper = tiempoParaRomperOriginal;
+    }
+
+    public void Update()
+    {
+        tiempoParaError -= Time.deltaTime;
+
+        if (tiempoParaError <= 0 && error == false) error = true;
+
+        if (error == true && roto == false) tiempoParaRomper -= Time.deltaTime;
+
+        if (tiempoParaRomper <= 0)
+        {
+            roto = true;
+            if (errorEjecutado == false)
+            {
+                gameObject.SendMessage("SistemaFallando");
+                errorEjecutado = true;
+            }
+        }
+    }
+
     public void OnTriggerEnter()
     {
-        ContenedorPiezas01 cont01= contenedorPieza01.GetComponentInChildren<ContenedorPiezas01>();
-        if (cont01.cantidadPieza > 0 && pieza01 == false)
+        if (error == true && roto == false)
         {
-            cont01.cantidadPieza--;
-            pieza01 = true;
+            ContenedorPiezas01 cont01 = contenedorPieza01.GetComponentInChildren<ContenedorPiezas01>();
+            if (cont01.cantidadPieza > 0 && pieza01 == false)
+            {
+                cont01.cantidadPieza--;
+                pieza01 = true;
+            }
+
+            if (pieza01 == true) tildePieza01.SetActive(true);
+
+            ContenedorPiezas01 cont02 = contenedorPieza02.GetComponentInChildren<ContenedorPiezas01>();
+            if (cont02.cantidadPieza > 0 && pieza02 == false)
+            {
+                cont02.cantidadPieza--;
+                pieza02 = true;
+            }
+
+            if (pieza02 == true) tildePieza02.SetActive(true);
+
+            ContenedorPiezas01 cont03 = contenedorPieza03.GetComponentInChildren<ContenedorPiezas01>();
+            if (cont03.cantidadPieza > 0 && pieza03 == false)
+            {
+                cont03.cantidadPieza--;
+                pieza03 = true;
+            }
+
+            if (pieza03 == true) tildePieza03.SetActive(true);
+
+            if (pieza01 == true && pieza02 == true && pieza03 == true)
+            {
+                error = false;
+                tiempoParaError = tiempoParaErrorOriginal--;
+                tiempoParaRomper = tiempoParaRomperOriginal--;
+
+                tildePieza01.SetActive(false);
+                tildePieza02.SetActive(false);
+                tildePieza03.SetActive(false);
+            }
+
+                Cartel.SetActive(true);
         }
-        
-        if (pieza01 == true) tildePieza01.SetActive(true);
-
-        ContenedorPiezas01 cont02 = contenedorPieza02.GetComponentInChildren<ContenedorPiezas01>();
-        if (cont02.cantidadPieza > 0 && pieza02 == false)
-        {
-            cont02.cantidadPieza--;
-            pieza02 = true;
-        }
-
-        if (pieza02 == true) tildePieza02.SetActive(true);
-
-        ContenedorPiezas01 cont03 = contenedorPieza03.GetComponentInChildren<ContenedorPiezas01>();
-        if (cont03.cantidadPieza > 0 && pieza03 == false)
-        {
-            cont03.cantidadPieza--;
-            pieza03 = true;
-        }
-        
-        if (pieza03 == true) tildePieza03.SetActive(true);
-
-        Cartel.SetActive(true);
     }
 
     public void OnTriggerExit()
---
+++ Assets/Scripts/SistemaFallandoGravedad01.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SistemaFallandoGravedad01 : MonoBehaviour
+{
+    public void SistemaFallando()
+    {
+        GameObject PJ = GameObject.FindGameObjectWithTag("Player");
+        SaltoPersonaje01 saltoPJ = PJ.GetComponentInChildren<SaltoPersonaje01>();
+
+        saltoPJ.FuerzaSalto--;
+    }
+}
