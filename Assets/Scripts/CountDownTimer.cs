a527723cb32e8c5fa4765ea512b693fa8ee3d76b
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-02-01 03:25:55 -0300

    Script CountDownTimer

---
+++ Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer : MonoBehaviour
+{
+    public Text txtCountDown;
+    public float timeQuest;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+
+    void Start()
+    {
+        timer = timeQuest;
+    }
+
+    void Update()
+    {
+        if (timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            txtCountDown.text = timer.ToString("F");
+        }
+        else if (timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            txtCountDown.text = "0.00";
+            timer = 0.0f;
+        }
+    }
+}
