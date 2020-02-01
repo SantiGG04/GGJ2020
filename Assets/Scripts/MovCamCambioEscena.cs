223731ba4af3ea5c84617d690d22db94d203d99a
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-02-01 10:41:50 -0300

    Script MovCamCambioEscena
    Script que busca y controla la Camara y el Canvas, para hacer la transición de Escena.

---
+++ Assets/Scripts/MovCamCambioEscena.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovCamCambioEscena : MonoBehaviour
+{
+    public Camera mainCam;
+    public float velocidadMovimiento;
+    public GameObject mainCanvas;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+        mainCanvas = GameObject.Find("Canvas");
+        velocidadMovimiento *= Time.deltaTime;
+        mainCanvas.GetComponent<Canvas>().enabled = false;
+    }
+
+    void Update()
+    {
+        mainCam.gameObject.transform.Translate(velocidadMovimiento, 0, 0);
+    }
+}
