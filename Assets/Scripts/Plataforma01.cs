45702938e24f25e19d27273996511ff54df687a9
SantiGG04 <60526538+SantiGG04@users.noreply.github.com>
2020-02-01 20:02:07 -0300

    Solucuonar Plataformas

---
+++ Assets/Scripts/Plataforma01.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataforma01 : MonoBehaviour
+{
+	public float impulsoSalto;
+
+ 	public void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.transform.parent.name == "Personaje")
+		{
+			Gravedad01 Grav = col.gameObject.GetComponentInChildren<Gravedad01>();
+			if (Grav.FuerzaVertical >= 0)
+			{
+				Debug.Log(Grav.FuerzaVertical);
+				Grav.FuerzaVertical += impulsoSalto;
+				Debug.Log(Grav.FuerzaVertical);
+
+				Grav.EnAire();
+			}
+		}
+	}
+}
