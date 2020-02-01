281a17db38dab147697ea4ff6c3dbb689b86a516
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-02-01 15:39:51 -0300

    Enemigo y MovimientoEnemigo

---
+++ Assets/Scripts/MovimientoEnemigos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoEnemigos : MonoBehaviour
+{
+	public float velocidadMovimiento;
+
+	public GameObject origenRayCasts;
+
+	public float distanciaRayCastL;
+	public float distanciaRayCastR;
+
+	void FixedUpdate()
+	{
+		RaycastHit lHit;
+		Ray lColision = new Ray(origenRayCasts.transform.position, Vector3.left);
+		Debug.DrawRay(origenRayCasts.transform.position, Vector3.left * distanciaRayCastL);
+
+		RaycastHit rHit;
+		Ray rColision = new Ray(origenRayCasts.transform.position, Vector3.right);
+		Debug.DrawRay(origenRayCasts.transform.position, Vector3.right * distanciaRayCastR);
+
+		if (Physics.Raycast(lColision, out lHit, distanciaRayCastL) || Physics.Raycast(rColision, out rHit, distanciaRayCastR))
+		{
+			velocidadMovimiento *= -1;
+		}
+		
+		transform.Translate(velocidadMovimiento * Time.deltaTime, 0, 0);
+	}
+}
