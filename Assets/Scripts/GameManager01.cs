3d27d66d0fedc17534c7ea44308b212526466085
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-02-02 14:14:32 -0300

    GameManager

--- Assets/Scripts/GameManager01.cs
+++ Assets/Scripts/GameManager01.cs
@@ -1,8 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager01 : MonoBehaviour
 {
     public int nivelDeRotura;
+
+    public float tiempoVictoria;
+
+    public Text txtCountDown;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+
+    public string escenaVictoria;
+    public string escenaDerrota;
+
+    void OnEnable()
+    {
+        timer = tiempoVictoria;
+    }
+
+    void Update()
+    {
+        if (timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            txtCountDown.text = timer.ToString("F");
+        }
+        else if (timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            txtCountDown.text = "0.00";
+            timer = 0.0f;
+            SceneManager.LoadScene(escenaVictoria);
+        }
+
+        if (nivelDeRotura == 3)
+        {
+            SceneManager.LoadScene(escenaDerrota);
+        }
+    }
 }
