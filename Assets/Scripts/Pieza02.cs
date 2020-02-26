6f012ef2e10c7a4da2c9a7c85857e8acc1e59710
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-02-25 21:55:54 -0300

    Sonido Piezas y Ajuste Camara Tutorial

--- Assets/Scripts/Pieza02.cs
+++ Assets/Scripts/Pieza02.cs
@@ -7,6 +7,16 @@ public class Pieza02 : MonoBehaviour
     public string nombrePieza;
     public GameObject contenedor;
 
+    // Variables de Sonido
+    [Header("Variables de Sonido")]
+    public GameObject contendedorSonidoAgarrarPieza;
+
+    private void Start()
+    {
+        // Inicializacion de Sonidos en false, para que no suenen si nos los olvidamos habilitados
+        contendedorSonidoAgarrarPieza.SetActive(false);
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.transform.gameObject.tag == "Player")
@@ -15,6 +25,8 @@ public class Pieza02 : MonoBehaviour
             if (cont.cantidadPieza < cont.cantidadMaxima)
             {
                 cont.cantidadPieza++;
+                contendedorSonidoAgarrarPieza.SetActive(false); // Primero se deshabilita, ya que luego de que suene por primera vez hay que dejarlo habilitado para que termine el sonido
+                contendedorSonidoAgarrarPieza.SetActive(true); // Habilitamos el contenedor para que suenen los sonidos que contiene
                 Destroy(gameObject);
             }
         }
