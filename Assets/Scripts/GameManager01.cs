7f45e8e002b357e25cfb2b419e2f267e2aa04dec
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-03-06 21:35:44 -0300

    Arreglando GameManager
    No funcionaba bien la Condicion de Victoria, ya está arreglada. Mejoré y acomodé un poco el Código. Lo dejé todo comentado.

--- Assets/Scripts/GameManager01.cs
+++ Assets/Scripts/GameManager01.cs
@@ -17,7 +17,7 @@ public class GameManager01 : MonoBehaviour
     public Text txtCountDown;
 
     [Header("Timer")]
-    private float timer;
+    private float timer = 1;
     private bool canCount = false;
     private bool doOnce = false;
     private bool doOnce2 = false;
@@ -34,7 +34,7 @@ public class GameManager01 : MonoBehaviour
     void Start()
     {
         MakeSingleton();
-        timer = tiempoVictoria;
+        txtCountDown.enabled = false; // Deshabilitamos el Text del Contador
     }
 
     void Update()
@@ -43,52 +43,39 @@ public class GameManager01 : MonoBehaviour
 
         if ((escenaActual.name == escenaJuego) && (!doOnce2))
         {
-            timer = tiempoVictoria;
-            doOnce2 = true;
-            canCount = true;
-            doOnce = false;
+            IniciarNivel();
         }
 
         if (timer >= 0.0f && canCount)
         {
             timer -= Time.deltaTime;
-            txtCountDown.text = timer.ToString("F");
+            txtCountDown.text = timer.ToString("F"); // Se muestra el Contador en el Objeto Text
         }
         else if (timer <= 0.0f && !doOnce)
         {
-            canCount = false;
-            doOnce = true;
-            txtCountDown.text = "0.00";
-            timer = 0.0f;
-            audioGameA.volume = 0.5f;
-            audioGameB.volume = 0.0f;
-            nivelDeRotura = 0;
-            doOnce2 = false;
-            SceneManager.LoadScene(escenaVictoria);
+            SalirDeNivel();
+            SceneManager.LoadScene(escenaVictoria); // Se carga la Escena Victoria
         }
 
         if (nivelDeRotura == 3)
         {
-            audioGameA.volume = 0.5f;
-            audioGameB.volume = 0.0f;
-            nivelDeRotura = 0;
-            doOnce2 = false;
-            SceneManager.LoadScene(escenaDerrota);
+            SalirDeNivel();
+            SceneManager.LoadScene(escenaDerrota); // Se carga la Escena Derrota
         }
         else if (nivelDeRotura == 2)
         {
-            audioGameA.volume = 0.0f;
-            audioGameB.volume = 0.5f;
+            audioGameA.volume = 0.0f; // Música OFF
+            audioGameB.volume = 0.5f; // Distorsión ON
         }
         else if (nivelDeRotura == 1)
         {
-            audioGameA.volume = 0.5f;
-            audioGameB.volume = 0.5f;
+            audioGameA.volume = 0.5f; // Música ON
+            audioGameB.volume = 0.5f; // Distorsión ON
         }
         else if (nivelDeRotura == 0)
         {
-            audioGameA.volume = 0.5f;
-            audioGameB.volume = 0.0f;
+            audioGameA.volume = 0.5f; // Música ON
+            audioGameB.volume = 0.0f; // Distorsión OFF
         }
     }
 
@@ -103,5 +90,31 @@ public class GameManager01 : MonoBehaviour
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    private void SalirDeNivel()
+    {
+        audioGameA.volume = 0.5f; // Subimos la Música
+        audioGameB.volume = 0.0f; // Bajaos la Distorsión
+ 
+        nivelDeRotura = 0; // Re inicializamos el Nivel de Rotura (Esto tiene que hacerse acá, sino al perder no carga la Escena siguiente)
+
+        doOnce = true; // Para que no vuelva a entrar a este IF
+        doOnce2 = false; // Lo ponemos en falso para que cuando carge la nueva escena vuelva a comprobar cuando se carga de nuevo el Nivel0001
+        canCount = false; // Para que no siga contando
+
+        timer = 0.0f; // Ponemos el Contador en cero
+
+        txtCountDown.enabled = false; // Deshabilitamos el Text del Contador
+        txtCountDown.text = ""; // Ponemos el Texto del Contador en vacio para que no se vea
+    }
+
+    private void IniciarNivel()
+    {
+        timer = tiempoVictoria;
+        txtCountDown.enabled = true;
+        doOnce2 = true;
+        canCount = true;
+        doOnce = false;
     }
 }
