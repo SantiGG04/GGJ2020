3d65e68a6df404e6e13647b79fac839c0176cffa
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-03-13 14:41:00 -0300

    Sistema de Iluminacion

--- Assets/Scripts/SistemaFallandoGravedad01.cs
+++ Assets/Scripts/SistemaFallandoGravedad01.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class SistemaFallandoGravedad01 : MonoBehaviour
 {
     public float porcentajePenalizacionVelocidad;
+
     public void SistemaFallando()
     {
         GameObject PJ = GameObject.FindGameObjectWithTag("Player");
         SaltoPersonaje01 saltoPJ = PJ.GetComponentInChildren<SaltoPersonaje01>();
 
-        saltoPJ.FuerzaSalto = saltoPJ.FuerzaSalto - ((saltoPJ.FuerzaSalto * porcentajePenalizacionVelocidad) / 100);
+        saltoPJ.FuerzaSalto -= ((saltoPJ.FuerzaSalto * porcentajePenalizacionVelocidad) / 100);
     }
 }
--- Assets/Scripts/SistemaFallandoIluminacion01.cs
+++ Assets/Scripts/SistemaFallandoIluminacion01.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SistemaFallandoIluminacion01 : MonoBehaviour
 {
+    public Image imagenError;
 
+    public void SistemaFallando()
+    {
+        imagenError.gameObject.SetActive(true);
+    }
 }
