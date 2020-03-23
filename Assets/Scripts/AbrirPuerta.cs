ec270ee2ad5423eca886e75f6515bfdb7fe1ec85
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-03-23 18:49:04 -0300

    Update AbrirPuerta.cs

--- Assets/Scripts/AbrirPuerta.cs
+++ Assets/Scripts/AbrirPuerta.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class AbrirPuerta : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Objetos")]
+    public GameObject puertaArriba;
+    public GameObject puertaAbajp;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
