8767160d665ca43bbae1a420f8b00965da91fd90
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-04-05 17:48:13 -0300

    Empece a pensar el Sistema de Puerta y cree el Script

---
+++ Assets/Scripts/SistemaFallandoPuertas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SistemaFallandoPuertas : MonoBehaviour
+{
+    // La idea que tengo es hacer una funcion que busque en la Escena los Objetos que tengan un Tag llamado "ColliderPuerta" y que lo active. Así podemos agregar puetas sin tener que cambiar el Script ni las Variables.
+
+    /*
+     
+    https://answers.unity.com/questions/24257/how-do-i-find-all-game-objects-with-the-same-name.html 
+
+    foreach(GameObject fooObj in GameObject.FindGameObjectsWithTag("foo"))
+    {
+        if(fooObj.name == "bar")
+        {
+            //Do Something
+        }
+    }
+    */
+}
